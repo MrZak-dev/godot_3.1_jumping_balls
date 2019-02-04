911e0d5d5b996eb9ae7473a61604b51302ef7538
MrZak-dev <zakaria.guenna@gmail.com>
2019-02-04 07:45:14 +0000

    init

---
+++ Ball.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+var GRAVITY = 55
+var ROTATION = 30
+var directions = [1 , -1]
+var sprite
+var raycast
+var velocity = Vector2(300, 1100)
+func _ready():
+	raycast = $topRC
+	sprite  = $Sprite
+	velocity.x *= directions[randi() % 2 ]  # direction could be 1 or -1 for reverse
+	ROTATION *= directions[randi() % 2 ]
+	pass
+func _physics_process(delta):
+	var collision_info = move_and_collide(velocity * delta)
+	if collision_info:
+		velocity = velocity.bounce(collision_info.normal)
+	if raycast.is_colliding():
+		velocity.y = min(velocity.y + GRAVITY , 1100)
+	sprite.rotation_degrees += ROTATION * delta ##ball rotation
