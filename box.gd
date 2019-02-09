0b1167a69509879cc7e98e62ea6170d8869cf0b9
B4DNetwork <b4dnetwork@gmail.com>
2019-02-09 16:50:23 +0000

    Update box.gd

--- box.gd
+++ box.gd
@@ -1,9 +1,5 @@
 extends KinematicBody2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 var gravity = Vector2(0,500)
 # Called when the node enters the scene tree for the first time.
 func _ready():
