9234c557184e27865fed4a970b48d0ce894e7ba2
MrZak-dev <zakaria.guenna@gmail.com>
2019-02-25 23:09:52 +0100

    smooth mouvement improving

--- scenes/player/player_cannon.gd
+++ scenes/player/player_cannon.gd
@@ -1,25 +1,35 @@
 extends KinematicBody2D
 
 var motion = Vector2()
-var moving_speed = 95
-var max_speed = 300
+var moving_speed = 230
+var max_speed = 400 
 var touch_point = Vector2()
 var pressed = false
 var player_position = Vector2()
 
+#dra variables
+var drag_index
+
 
 func _process(delta):
-	player_position.x = int(self.get_position().x)
-	if player_position.x > touch_point.x:
-		motion.x = min(motion.x - moving_speed , -max_speed )
-	if player_position.x < touch_point.x:
-		motion.x = max(motion.x + moving_speed, max_speed)
-	move_and_slide(motion)
-	#pressed = false #in the end of each frame make it false so the movements wont keep going
+	if pressed:
+		player_position.x = int(self.get_position().x)
+		if player_position.x > touch_point.x:
+			motion.x = min(motion.x - moving_speed,-max_speed)
+		if player_position.x < touch_point.x:
+			motion.x = max(motion.x + moving_speed,max_speed)
+		move_and_slide(motion)
+		pressed = false #in the end of each frame make it false so the movements wont keep going
+	else:
+		move_and_slide(Vector2(0,0))
 	pass  
 func _input(event):
-	if (event is InputEventScreenTouch) or (event is InputEventScreenDrag):
+	if event is InputEventScreenDrag or InputEventScreenTouch:
+		if event is InputEventScreenDrag: #fonctions that works only for drag event
+			max_speed = event.get_speed().x
+			drag_index = event.get_index()
+			pass
 		pressed = true #if an input event received start moving the canon
 		touch_point.x = int(event.get_position().x)
-#TODO : figure out the shaking phwsics where it is coming from 
+#TODO : figure out the shaking physics where it is coming from 
 #Notice : this scene is working only on android devices
