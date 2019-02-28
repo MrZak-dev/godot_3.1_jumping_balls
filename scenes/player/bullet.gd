733edf5b05619419f8011361f4df110d507bab65
MrZak-dev <zakaria.guenna@gmail.com>
2019-02-28 18:17:13 +0100

    player is firing bullets now with some fire animation

---
+++ scenes/player/bullet.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+
+const SPEED = -3000
+var velocity = Vector2()
+func _ready():
+	pass
+
+func _physics_process(delta):
+	velocity.y = SPEED * delta
+	translate(velocity)
+	pass
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
--- scenes/player/tween_player.gd
+++ scenes/player/tween_player.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 onready var tween_node = get_node("Tween")
-
+const BULLET = preload("res://scenes/player/bullet.tscn")
 func _ready():
 	pass # Replace with function body.
 
@@ -11,4 +11,15 @@ func _physics_process(delta):
 	if Input.is_mouse_button_pressed(BUTTON_LEFT):
 		tween_node.interpolate_property(self,"position:x",get_position().x,click_position.x,0.75,Tween.TRANS_EXPO,Tween.EASE_OUT)
 		tween_node.start()
+		
+		#loading bullet 
+		var one_bullet = BULLET.instance()
+		get_parent().add_child(one_bullet)
+		one_bullet.position = $Position2D.global_position
+		
+		#play fire animation
+		$AnimatedSprite.play("fire")
+	else:
+		tween_node.stop_all()
+		$AnimatedSprite.stop()
 	pass
